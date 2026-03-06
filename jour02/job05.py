e09a4bcb87bcd081d52ff6eed69e43b3f505bd6a
noemie-feraud <noemie.feraud@laplateforme.io>
2026-03-06 15:13:29 +0100

    feat:jour02 add job05.py

---
+++ jour02/job05.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host = "127.0.0.1",
+    user = "root",
+    password = "pr4M369w!",
+    database = "LaPlateforme"
+)
+
+cursor = connection.cursor()
+cursor.execute("SELECT SUM(superficie) FROM etage")
+resultat = cursor.fetchone()[0]
+print(f"La superficie de La Plateforme est de {resultat} m2")
+
+cursor.close()
+connection.close()
