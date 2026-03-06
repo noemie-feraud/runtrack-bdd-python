fee4966fec6cf793ffa6b97eef70a9c778f74590
noemie-feraud <noemie.feraud@laplateforme.io>
2026-03-06 12:28:16 +0100

    feat:jour02 add job03.py

---
+++ jour02/job03.py
@@ -0,0 +1,21 @@
+import mysql.connector
+import pandas as pd
+
+connection = mysql.connector.connect(
+    host = "127.0.0.1",
+    user = "root",
+    password = "pr4M369w!" ,
+    database = "LaPlateforme"
+)
+
+data1 = "SELECT * FROM LaPlateforme.etage "
+df1 = pd.read_sql(data1, connection)
+
+data2 = "SELECT * FROM LaPlateforme.salle"
+df2 = pd.read_sql(data2, connection)
+
+print("--- DATAFRAME ETAGE ---")
+print(df1)
+
+print("--- DATAFRAME SALLE ---")
+print(df2)
